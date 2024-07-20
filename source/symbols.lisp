b27b6311ba6c93d8ff58695824fc399518d68cc2
daedsidog <41439659+daedsidog@users.noreply.github.com>
2024-07-21 02:09:29 +0300

    Prevent EXPORT* from trying to export inaccessible symbols

--- source/symbols.lisp
+++ source/symbols.lisp
@@ -94,10 +94,14 @@ want to be exported."
      (export ',name)))
 
 (defmacro export* (&rest source-packages)
-  "Export the public symbols of SOURCE-PACKAGES to the current package's symbol table."
+  "Export the public symbols of SOURCE-PACKAGES to the current package's symbol table.
+
+Exports only if the symbol is accessible in the current package."
   `(dolist (source-package ',source-packages)
      (loop for symbol being the external-symbols of source-package
-           do (export symbol ,(package-name *package*)))))
+           do (multiple-value-bind (symbol source) (find-symbol (symbol-name symbol) *package*)
+                (unless (eq source :internal)
+                  (export symbol ,(package-name *package*)))))))
 
 (defun keywordicate (symbol)
   "Intern SYMBOL into the keyword package and return it."
