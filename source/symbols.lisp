6d3c3d2bafcb4d3fbad19147c1e159ec704ced3f
daedsidog <41439659+daedsidog@users.noreply.github.com>
2024-07-24 18:41:04 +0300

    Fix bad iteration for EXPORT-INHERITED-SYMBOLS

--- source/symbols.lisp
+++ source/symbols.lisp
@@ -10,15 +10,16 @@
 
 Exports only if the symbol is accessible in the current package."
   `(progn
-     (dolist (source-package ,source-packages)
+     (dolist (source-package ',source-packages)
        (loop for symbol being the external-symbols of source-package
              do (multiple-value-bind (symbol source) (find-symbol (symbol-name symbol) *package*)
                   (unless (eq source :internal)
                     (export symbol ,(package-name *package*))))))
-     (loop for symbol being the internal-symbols of *package*
-           do (dolist (source-package ,source-packages)
-                (when (member (symbol-package symbol) ,source-packages)
-                  (export symbol ,(package-name *package*)))))))
+     (do-symbols (symbol *package*)
+       do (dolist (source-package ',source-packages)
+            (setf source-package (find-package source-package))
+            (when (eq (symbol-package symbol) source-package)
+              (export symbol ,(package-name *package*)))))))
 
 (defun keywordicate (symbol)
   "Intern SYMBOL into the keyword package and return it."
