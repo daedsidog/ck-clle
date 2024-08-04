1fc9077f19c262a6b7c244d840ad97b0912a518f
daedsidog <41439659+daedsidog@users.noreply.github.com>
2024-08-05 02:34:57 +0300

    Add conventional predicates for ATOM and NULL

--- source/clle.lisp
+++ source/clle.lisp
@@ -3,6 +3,14 @@
   (:use-reexport #:ck-clle/symbols
                  #:ck-clle/collections
                  #:ck-clle/types
-                 #:iterate))
+                 #:iterate)
+  (:lock t))
+
+(in-package #:iterate)
+
+(remprop 'count 'synonym)
 
 (in-package #:ck-clle)
+
+(setf (fdefinition 'atomp) (fdefinition 'atom))
+(setf (fdefinition 'nullp) (fdefinition 'null))
